cfdbacb3e8c722c355507127faf33020a1d59426
Manoj singh <manojpatelia@gmail.com>
2024-07-15 00:35:53 +0530

    changes for spring security using in memory users

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	    @Bean
+	    public PasswordEncoder passwordEncoder() {
+	    	
+	    	return new BCryptPasswordEncoder();
+	    }
+	    
+	    @Bean
+	    public UserDetailsService userDetailsService() {
+	    	
+	    	UserDetails user1 = User.withUsername("manoj")
+	    						   .password(passwordEncoder().encode("manoj"))
+	    						   .roles("USER")
+	    						   .build();
+	    	
+	    	UserDetails user2 = User.withUsername("paplu")
+					   .password(passwordEncoder().encode("paplu"))
+					   .roles("ADMIN")
+					   .build();
+	    	
+	    	return new InMemoryUserDetailsManager(user1,user2);
+	    }
+	
+	    @Bean
+		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+			
+				http.authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+					.httpBasic(Customizer.withDefaults());
+				
+				return http.build();
+		}
+}
